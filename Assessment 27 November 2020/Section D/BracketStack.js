bf911019ba83e99392801642af6cade552df0a87
Leon Stevens <32592070+LeonStevens122@users.noreply.github.com>
2020-12-01 16:43:56 +0200

    Added Section D

--- Assessment 27 November 2020/Section D/BracketStack.js
+++ Assessment 27 November 2020/Section D/BracketStack.js
@@ -1,3 +1,19 @@
+/*
+The Time and Space complexity of this function is describe using Big "O" Notation
+
+Time Complexity - O(n)
+The  solution iterates the length of the input string, so our time cost will grow in linear proportion to the growth of the string length.
+ Or, for each character that is added to the input string, the algorithm will take 1 time unit longer to complete (worst case).
+ All other operations in our algorithm are constant time because we are using object-property lookup to find our comparison 
+ values and the pop method of a Stack data structure to keep track of all the parenthesis pairs that get opened.
+
+Space complexity is also o(n)
+because of the way the stack is constructed. if there are only opening brackets
+and no closing brackets in the string. the BracketStack would end up being the same length as the 
+input string
+
+*/
+
 function BracketStack() {
   let openBracketsCount = 0;
   this.isEmpty = function () {
@@ -6,15 +22,16 @@ function BracketStack() {
   this.push = function () {
     openBracketsCount++;
   };
+
   /**
    * @throws If stack is empty
    */
+
   this.pop = function () {
     if (this.isEmpty()) throw new Error("Cannot pop empty stack");
     openBracketsCount--;
   };
 }
-
 
 /**
  * @param {string} symbol
@@ -26,13 +43,12 @@ function isSymbol(symbol) {
    * @param {string} value
    * @returns {boolean}
    */
-  
+
   function check(value) {
     return symbol === value;
   }
   return check;
 }
-
 
 /**
  * Checks if a pair of brackets match
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Cogrammer Assessment November 2020
 
 ## Leon Stevens
+
+The Section B Full Stack application has been deployed to
+https://leon-mern-blog.herokuapp.com/
